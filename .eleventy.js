1a9de2c7774c14f37887a586d2952c520544d6f9
elFani <elFani@users.noreply.github.com>
2022-01-13 14:32:09 -0800

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const pluginCloudCannonBookshop = require("@bookshop/cloudcannon-eleventy-bookshop");
+
+module.exports = function (eleventyConfig) {
+
+	eleventyConfig.htmlTemplateEngine = "liquid";
+
+	eleventyConfig.addPlugin(pluginBookshop({
+		bookshopLocations: ["component-library"]
+	}));
+	eleventyConfig.addPlugin(pluginCloudCannonBookshop);
+
+	eleventyConfig.addPassthroughCopy('site/assets');
+	eleventyConfig.addPassthroughCopy('site/_cloudcannon');
+
+	return {
+		dir: {
+			input: 'site',
+			pages: 'pages'
+		}
+	}
+};
